fc8e79f50516a31a758de2f2761ea927f8b83444
RaphaelVispo <rsvispo@up.edu.ph>
2023-05-13 15:36:08 +0800

    Refactor: code for linear regression

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,85 @@
 import React , {useState, useEffect} from 'react'
 import Plot from 'react-plotly.js';
+import {solve_linear_regression} from './linear_regression'
 
 
 function App(){
-  const [data, setData] = useState([{}]);
+  const [data_Supply, setData_Supply] = useState({
+    original: {
+      X: [0, 1, 2, 3, 4],
+      y: [0, 1, 4, 9, 16],
+    },
+    regression: {
+      X: [0, 1, 2, 3, 4],
+      y: [0, 1, 4, 9, 16],      
+    }
 
-  const solve_linear_regression = async () => {
-    const res = await fetch("/api/linear_regression", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        X: [0, 1, 2, 3, 4],
-        y: [0, 1, 4, 9, 16],
-      }),
-    });
-  
-    const res_data = await res.json()
-    const json_data =  JSON.parse(res_data );
-    setData(json_data);
-  };
+  });
+  const [data_Demand, setdata_Demand] = useState({
+    original: {
+      X: [0, 1, 2, 3, 4],
+      y: [16, 9, 4, 1, 0]
+    },
+    regression: {
+      X: [0, 1, 2, 3, 4],
+      y: [0, 1, 4, 9, 16],      
+    }
+
+  });
+
   
   useEffect(() => {
-    solve_linear_regression();
+    solve_linear_regression(setData_Supply, data_Supply);
+    solve_linear_regression(setdata_Demand, data_Demand);
+
   }, []);
   
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    console.log("supply:", data_Supply);
+  }, [data_Supply]);
+
+  useEffect(() => {
+    console.log("demand:", data_Demand);
+  }, [data_Demand]);
 
 
   return(
     <div>
       <Plot
         data={[
+
           {
-            x: data.X,
-            y: data.y,
+            x: data_Supply.regression.X  ,
+            y: data_Supply.regression.y ,
             type: 'line',
             marker: {color: 'red'},
+            line: { width: 5} ,
+            name: 'Supply Regression',
+          },
+          {
+            x: data_Demand.regression.X  ,
+            y: data_Demand.regression.y ,
+            type: 'line',
+            marker: {color: 'blue'},
+            line: { width: 5} ,
+            name: 'Demand Regression',
           },
           {type: 'scatter',
-            x: [0, 1, 2, 3, 4], 
-            y: [0, 1, 4, 9, 16],
+            x: data_Supply.original.X,
+            y: data_Supply.original.y,
             mode: 'markers',
+            marker: {opacity: 0.5},
+            name: 'Supply Data',
+          },
+          {type: 'scatter',
+            x: data_Demand.original.X,
+            y: data_Demand.original.y,
+            mode: 'markers',
+            marker: {opacity: 0.5},
+            name: 'Demand Data',
           },
         ]}
-        layout={{width: 1000, height: 1000, title: 'A Fancy Plot'}}
+        layout={{width: 1000, height: 1000}}
         ></Plot>
     </div>
   )
