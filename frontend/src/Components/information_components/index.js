3bf23ec637a64c3a8c7f7b40753e2859dd4491d1
RaphaelVispo <rsvispo@up.edu.ph>
2023-06-19 15:27:40 +0800

    Fix: prevent the reloading of info

--- frontend/src/Components/information_components/index.js
+++ frontend/src/Components/information_components/index.js
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import {Container, Navbar, Nav, Row, Col, Card, Button} from 'react-bootstrap';
+import React, { useState } from 'react';
+import {Nav, Card} from 'react-bootstrap';
 import {
     BrowserRouter,
     Routes,
@@ -8,41 +8,53 @@ import {
   } from "react-router-dom";
 import {Law_Of_Demand} from "./law_of_demand";
 import {Law_of_Supply} from "./law_of_supply";
+import { Matrix_Input } from '../matrix_input/matrix_input';
 
 
 
 export function Card_information(props) {
     const { data_slider, adjust_slider, data_Demand, adjust_slope_slider, slope_slider ,shift_slider, adjust_shift_slider} =  props
+    const [info, setInfo] = useState("Matrix_Input")
+
+    const change_info  = (value) => {
+        setInfo(value)
+    }
 
     return (
         <Card id="info">
         <Card.Header>
         <Nav variant="pills" defaultActiveKey="#first">
             <Nav.Item>
-            <Nav.Link href="/matrix" >Matrix</Nav.Link>
+            <Nav.Link onClick={()=>{ change_info("Matrix_Input")}}>Matrix</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-            <Nav.Link href="/law_of_demand">Law of Demand</Nav.Link>
+            <Nav.Link onClick={()=>{ change_info("Law_of_Demand")}}>Law of Demand</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-            <Nav.Link href="/law_of_supply">Law of Supply</Nav.Link>
+            <Nav.Link onClick={()=>{ change_info("Law_of_Supply")}}>Law of Supply</Nav.Link>
             </Nav.Item>
         </Nav>
         </Card.Header>
-        <BrowserRouter>
-        <Routes>
-        <Route path="/law_of_demand" element={<Law_Of_Demand
-                    shift_slider = {shift_slider}
-                    adjust_shift_slider  = {adjust_shift_slider}
-                    slope_slider = {slope_slider}
-                    data_Demand = {data_Demand}
-                    data_slider = {data_slider}
-                    adjust_slider = {adjust_slider}
-                    adjust_slope_slider = {adjust_slope_slider}
-        />} />
-        <Route path="/law_of_supply" element={<Law_of_Supply />} />
-        </Routes>
-        </BrowserRouter>
+        {
+            (info === "Matrix_Input") && <Matrix_Input/>
+
+        }
+        {
+            (info ===  "Law_of_Demand") && <Law_Of_Demand
+                shift_slider = {shift_slider}
+                adjust_shift_slider  = {adjust_shift_slider}
+                slope_slider = {slope_slider}
+                data_Demand = {data_Demand}
+                data_slider = {data_slider}
+                adjust_slider = {adjust_slider}
+                adjust_slope_slider = {adjust_slope_slider}
+             />
+        }
+                {
+            (info ===  "Law_of_Supply") && <Law_of_Supply />
+        }
+
+
         </Card>
     )
 }
---
+++ frontend/src/Components/matrix_input/matrix_input.js
@@ -0,0 +1,10 @@
+
+
+export function Matrix_Input (){
+    return (
+        <>
+            <h2>Input Value</h2>
+        </>
+    )
+
+}
