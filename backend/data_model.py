7562581e80af33a3d20c01a45dea99d51b075bb6
RaphaelVispo <rsvispo@up.edu.ph>
2023-06-20 20:52:31 +0800

    Add: poinst to the pydantics

--- backend/data_model.py
+++ backend/data_model.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
+from typing import Dict, List
 
-class Data(BaseModel):
-    X: list[int]
-    y: list[int]
+class point (BaseModel):
+    qd : List[int]
+    price : List[int]
+
+class type (BaseModel):
+    original: point
+    regression: point
+
+class data(BaseModel):
+    demand : type
+    supply : type
     slope: int = 0
     shift: int = 0
-
